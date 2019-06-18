3cc09575c7347e8591357ed5a3f8b0ea94283d6a
Kathy * <Kathy@KathyLambert.me>
2019-06-18 17:00:26 -0400

    Initial commit. Customized the index.html template for the project repo. Added Bootstrap CDN. Added card images.

---
+++ index.js
@@ -0,0 +1,47 @@
+// 1. Initialize Variables
+
+// let cards = document.querySelectorAll('memoryCard)
+// console.log(cards)
+
+// let isCardFlipped = false
+// let firstCard
+// let secondCard
+
+//  etc ...
+
+// create an array of cards
+
+// Assign a random number dataset.
+  // let id = randomly assigned #
+  // Use temperate literals to appended to the id in html.
+
+// take array cards and shuffle them randomly using math.Number() 
+
+
+
+// Take newly created array and display 
+
+
+
+// shuffleBoard() { //IIFE
+
+// }
+
+// function flipCard() {
+//  if (firstCard.onClick === false)
+// }
+// if onClick
+
+// first card selection
+// check to see if the dataAtt is the same  dataAtt === dataAtt; if so then secondCard != selected card.
+
+
+
+
+// function cardFlipped() {
+// let card = .memoryCard 
+// when .memoryCard is clicked check to see if there is already a firstCard.  If not, assign .this to firstCard and display card using `backface-visibility`.  Else assign .this to secondCard and display card using `backface-visibility`.
+// }
+
+
+
