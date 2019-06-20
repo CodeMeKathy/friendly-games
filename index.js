629dcc63c863e925016b765feb95db4e1d5e91b1
Kathy * <Kathy@KathyLambert.me>
2019-06-19 21:19:29 -0400

    Created an array of cards. Created a createBoard() which renders a 4x4 grid with CSS styling in the #gameBoard element.

--- index.js
+++ index.js
@@ -1,3 +1,91 @@
+// Option I
+
+// Create an array of cards with card objects
+
+let cards = [
+	{
+		id: 'card1',
+		cardFrontImage: 'memGame/friendly-games/images/memoryCards/accessible-icon-brands.svg'
+	},
+	{
+		id: 'card2',
+		cardFrontImage: 'images/memoryCards/angrycreative-brands.svg'
+	},
+	{
+		id: 'card3',
+		cardFrontImage: 'images/memoryCards/asterisk-solid.svg'
+	},
+	{
+		id: 'card4',
+		cardFrontImage: 'memGame/friendly-games/images/memoryCards/balance-scale-right-solid.svg'
+	},
+	{
+		id: 'card5',
+		cardFrontImage: 'memGame/friendly-games/images/memoryCards/brain-solid.svg'
+	},
+	{
+		id: 'card6',
+		cardFrontImage: 'memGame/friendly-games/images/memoryCards/cloud-sun-rain-solid.svg'
+	},
+	{
+		id: 'card7',
+		cardFrontImage: 'memGame/friendly-games/images/memoryCards/cocktail-solid.svg'
+	},
+	{
+		id: 'card8',
+		cardFrontImage: 'memGame/friendly-games/images/memoryCards/accessible-icon-brands.svg'
+	},
+	{
+		id: 'card9',
+		cardFrontImage: 'images/memoryCards/angrycreative-brands.svg'
+	},
+	{
+		id: 'card10',
+		cardFrontImage: 'images/memoryCards/asterisk-solid.svg'
+	},
+	{
+		id: 'card11',
+		cardFrontImage: 'memGame/friendly-games/images/memoryCards/balance-scale-right-solid.svg'
+	},
+	{
+		id: 'card12',
+		cardFrontImage: 'memGame/friendly-games/images/memoryCards/brain-solid.svg'
+	},
+	{
+		id: 'card13',
+		cardFrontImage: 'memGame/friendly-games/images/memoryCards/cloud-sun-rain-solid.svg'
+	},
+	{
+		id: 'card14',
+		cardFrontImage: 'memGame/friendly-games/images/memoryCards/cocktail-solid.svg'
+	},
+	{
+		id: 'card15',
+		cardFrontImage: 'memGame/friendly-games/images/memoryCards/cloud-sun-rain-solid.svg'
+	},
+	{
+		id: 'card16',
+		cardFrontImage: 'memGame/friendly-games/images/memoryCards/cocktail-solid.svg'
+	}
+]
+
+// Create an array for the cards currently in play.
+let cardsInPlay = []
+
+// Create game board
+let createBoard = () => {
+	for (let i = 0; i < 16; i++) {
+		let cardElement = document.createElement('img')
+		cardElement.setAttribute('data-id', [i])
+		document.getElementById('gameBoard').appendChild(cardElement)
+	}
+}
+
+console.log(cardsInPlay)
+createBoard()
+
+// Option II
+
 // 1. Initialize Variables
 
 // let cards = document.querySelectorAll('memoryCard)
@@ -12,16 +100,12 @@
 // create an array of cards
 
 // Assign a random number dataset.
-  // let id = randomly assigned #
-  // Use temperate literals to appended to the id in html.
+// let id = randomly assigned #
+// Use temperate literals to appended to the id in html.
 
-// take array cards and shuffle them randomly using math.Number() 
+// take array cards and shuffle them randomly using math.Number()
 
-
-
-// Take newly created array and display 
-
-
+// Take newly created array and display
 
 // shuffleBoard() { //IIFE
 
@@ -35,13 +119,7 @@
 // first card selection
 // check to see if the dataAtt is the same  dataAtt === dataAtt; if so then secondCard != selected card.
 
-
-
-
 // function cardFlipped() {
-// let card = .memoryCard 
+// let card = .memoryCard
 // when .memoryCard is clicked check to see if there is already a firstCard.  If not, assign .this to firstCard and display card using `backface-visibility`.  Else assign .this to secondCard and display card using `backface-visibility`.
 // }
-
-
-
